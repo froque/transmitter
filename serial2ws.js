6e1718ab55b40cfdbbe0cc3fada4ab7da7dae2c5
Filipe Roque <flip.roque@gmail.com>
2013-01-17 03:20:40 +0000

    improving the index.html
    this commit is not very well tested. just commiting to keep track of changes

--- serial2ws.js
+++ serial2ws.js
@@ -2,24 +2,24 @@ var sess = null;
 var wsuri = "ws://" + window.location.hostname + ":8081";
 var retryCount = 0;
 var retryDelay = 2;
-var interval;
+var winterval;
 
 // Callback used in PubSub for updating Transmitter Status
 function TransmitterStatus(topicUri, event) {
-    AlarmCode.innerHTML = event.AlarmCode
-    Pfwd.innerHTML = event.Pfwd
-    ref.innerHTML = event.ref
-    Texc.innerHTML = event.Texc
-    Tamp.innerHTML = event.Tamp
-    Uexc.innerHTML = event.Uexc
-    Uamp.innerHTML = event.Uamp
-    audio.innerHTML = event.Audio
-    Iexc.innerHTML = event.Iexc
-    Iamp.innerHTML = event.Iamp
-    Uptime.innerHTML = event.Uptime
-    Freq.innerHTML = event.Freq
-    Firmware.innerHTML = event.Firmware
-    PowerLimit.innerHTML = event.PowerLimit
+    AlarmCode.innerHTML = event.AlarmCode;
+    Pfwd.innerHTML = event.Pfwd;
+    ref.innerHTML = event.ref;
+    Texc.innerHTML = event.Texc;
+    Tamp.innerHTML = event.Tamp;
+    Uexc.innerHTML = event.Uexc;
+    Uamp.innerHTML = event.Uamp;
+    audio.innerHTML = event.Audio;
+    Iexc.innerHTML = event.Iexc;
+    Iamp.innerHTML = event.Iamp;
+    Uptime.innerHTML = event.Uptime;
+    Freq.innerHTML = event.Freq;
+    Firmware.innerHTML = event.Firmware;
+    PowerLimit.innerHTML = event.PowerLimit;
 }
 
 function readTX() {
@@ -27,13 +27,35 @@ function readTX() {
 }
 
 function autoReadTx() {
-  if (autoread.checked == true) {
+  if (autoread.checked ) {
     readTx.disabled = true;
-    interval = window.setInterval(readTX,2000);
+    winterval = window.setInterval(readTX,2000);
   } else {
     readTx.disabled = false;
-    window.clearInterval(interval);
+    window.clearInterval(winterval);
   }
+}
+
+function setSettings() {
+
+//  channels
+  attack_v = attack.options[attack.selectedIndex].value;
+  decay_v = decay.options[decay.selectedIndex].value;
+  interval_v = interval.options[interval.selectedIndex].value;
+  threshold_v = threshold.options[threshold.selectedIndex].value;
+  compression_v = compression.options[compression.selectedIndex].value;
+  swr_alarm_v = swr_alarm.options[swr_alarm.selectedIndex].value;
+  current_alarm_v = current_alarm.options[current_alarm.selectedIndex].value;
+  temp_alarm_v = temp_alarm.options[temp_alarm.selectedIndex].value;
+  Uamp_alarm_v = Uamp_alarm.options[Uamp_alarm.selectedIndex].value;
+  treble_v = treble.options[treble.selectedIndex].value;
+  bass_v = bass.options[bass.selectedIndex].value;
+  left_gain_v = left_gain.options[left_gain.selectedIndex].value;
+  right_gain_v = right_gain.options[right_gain.selectedIndex].value;
+
+  sess.call("rpc:set-settings",power.value,attack_v,decay_v,interval_v,
+            threshold_v,compression_v,swr_alarm_v,current_alarm_v,temp_alarm_v,
+            Uamp_alarm_v,treble_v,bass_v,left_gain_v,right_gain_v);
 }
 
 function connect() {
@@ -43,15 +65,18 @@ function connect() {
       function() {
          statusline.innerHTML = "Connected to " + wsuri;
          retryCount = 0;
-         sess.prefix("event", "http://example.com/mcu#");         // Prefix for CURIE
+         // Prefix for CURIE
+         sess.prefix("event", "http://example.com/mcu#");
          sess.subscribe("event:tx-status", TransmitterStatus);
-         sess.prefix("rpc", "http://example.com/mcu-control#");   // Prefix for CURIE
+         // Prefix for CURIE
+         sess.prefix("rpc", "http://example.com/mcu-control#");
       },
       // function used after close, lost, etc session
       function() {
          console.log(retryCount);
          retryCount = retryCount + 1;
-         statusline.innerHTML = "Connection lost. Reconnecting (" + retryCount + ") in " + retryDelay + " secs ..";
+         statusline.innerHTML = "Connection lost. Reconnecting (" + retryCount
+                                 + ") in " + retryDelay + " secs ..";
          window.setTimeout(connect, retryDelay * 1000);
       }
    );
--- serial2ws.py
+++ serial2ws.py
@@ -17,14 +17,15 @@
 ###############################################################################
 
 
-import sys, time, re
+import sys
+import re
 
 if sys.platform == 'win32':
-   ## on windows, we need to use the following reactor for serial support
-   ## http://twistedmatrix.com/trac/ticket/3802
-   ##
-   from twisted.internet import win32eventreactor
-   win32eventreactor.install()
+    ## on windows, we need to use the following reactor for serial support
+    ## http://twistedmatrix.com/trac/ticket/3802
+    ##
+    from twisted.internet import win32eventreactor
+    win32eventreactor.install()
 
 from twisted.internet import reactor
 print "Using Twisted reactor", reactor.__class__
@@ -41,37 +42,105 @@ from autobahn.wamp import WampServerFactory, WampServerProtocol, exportRpc
 
 
 class Serial2WsOptions(usage.Options):
-   optParameters = [
+    optParameters = [
       ['baudrate', 'b', 19200, 'Serial baudrate'],
       ['port', 'p', '/dev/ttyUSB0', 'Serial port to use'],
       ['webport', 'w', 8080, 'Web port to use for embedded Web server'],
-      ['wsurl', 's', "ws://localhost:9000", 'WebSocket port to use for embedded WebSocket server']
-   ]
+      ['wsurl', 's', "ws://localhost:9000",\
+                    'WebSocket port to use for embedded WebSocket server']
+    ]
 
 
 ## MCU protocol
 ## knows nothing about websockets
 ##
 class McuProtocol(LineReceiver):
-   delimiter = '$'
-   ## need a reference to our WS-MCU gateway factory to dispatch PubSub events
-   ##
-   def __init__(self, wsMcuFactory):
-      self.wsMcuFactory = wsMcuFactory
+    delimiter = '$'
+    ## need a reference to our WS-MCU gateway factory to dispatch PubSub events
+    ##
 
-   ## this method is exported as RPC and can be called by connected clients
-   ##
-   @exportRpc("read-tx")
-   def readTX(self):
-         print "DEBUG Read from transmitter"
-         self.transport.write('\x00\x46\x4d\x01\x20\x02')
+    def __init__(self, wsMcuFactory):
+        self.wsMcuFactory = wsMcuFactory
 
-   def connectionMade(self):
-      log.msg('Serial port connected.')
+    ## this method is exported as RPC and can be called by connected clients
+    ##
+    @exportRpc("read-tx")
+    def readTX(self):
+        print "DEBUG Read from transmitter"
+#        self.transport.write('\x00\x46\x4d\x01\x20\x02')
+        self.transport.write('\x00' + "FM" + '\x01' + '\x20' + '\x02')
 
-   def lineReceived(self, line):
-      print "DEBUG line", line
-      try:
+    @exportRpc("set-settings")
+    def setSettings(self, power, attack, decay, interval, threshold,
+                 compression, swr_alarm, current_alarm, temp_alarm,
+                 Uamp_alarm, treble, bass, left_gain, right_gain):
+        print power, attack, decay, interval, threshold, compression,\
+           swr_alarm, current_alarm, temp_alarm, Uamp_alarm, treble, bass,\
+           left_gain, right_gain
+
+    def setPower(self, power):
+        self.transport.write('\x00' + "FO" + '\x01' +
+                       str(int(round(int(power) / 100.0 * 34) + 4)) + '\x02')
+#   def setChannels(self, channels):
+#      self.transport.write('\x00' + "FS" + '\x01' + channels + '\x02')
+
+#   def setFrequency(self, freq):
+#      self.transport.write('\x00' + "FO" + '\x01' + freq + '\x02')
+
+    def setTreble(self, treble):
+        self.transport.write('\x00' + "FDT" + '\x01' +
+                                            str(int(treble) + 4) + '\x02')
+
+    def setBass(self, bass):
+        self.transport.write('\x00' + "FDB" + '\x01' +
+                                            str(int(bass) + 4) + '\x02')
+
+    def setAttack(self, attack):
+        self.transport.write('\x00' + "FDA" + '\x01' +
+                                            str(int(attack) + 4) + '\x02')
+
+    def setDecay(self, decay):
+        self.transport.write('\x00' + "FDD" + '\x01' +
+                                            str(int(decay) + 4) + '\x02')
+
+    def setThreshold(self, threshold):
+        self.transport.write('\x00' + "FDH" + '\x01' +
+                                            str(int(threshold) + 4) + '\x02')
+
+    def setIntegration(self, integration):
+        self.transport.write('\x00' + "FDI" + '\x01' +
+                                            str(int(integration) + 4) + '\x02')
+
+    def setLeftGain(self, leftGain):
+        self.transport.write('\x00' + "FDGL" + '\x01' +
+                                            str(int(leftGain) + 4) + '\x02')
+
+    def setRightGain(self, rightGain):
+        self.transport.write('\x00' + "FDGR" + '\x01' +
+                                            str(int(rightGain) + 4) + '\x02')
+
+    def setTempAlarm(self, tempAlarm):
+        self.transport.write('\x00' + "FAT" + '\x01' +
+                                            str(int(tempAlarm) + 4) + '\x02')
+
+    def setSWRAlarm(self, SWRAlarm):
+        self.transport.write('\x00' + "FAS" + '\x01' +
+                                            str(int(SWRAlarm) + 4) + '\x02')
+
+    def setUampAlarm(self, UampAlarm):
+        self.transport.write('\x00' + "FAU" + '\x01' +
+                                              str(int(UampAlarm) + 4) + '\x02')
+
+    def setIampAlarm(self, IampAlarm):
+        self.transport.write('\x00' + "FAC" + '\x01' +
+                                            str(int(IampAlarm) + 4) + '\x02')
+
+    def connectionMade(self):
+        log.msg('Serial port connected.')
+
+    def lineReceived(self, line):
+        print "DEBUG line", line
+        try:
             AlarmCode = int(re.search('AlarmCode=(.+?),', line).group(1))
             Pfwd = float(re.search('Pfwd=(.+?)W,', line).group(1))
             ref = float(re.search('ref=(.+?)W,', line).group(1))
@@ -87,13 +156,14 @@ class McuProtocol(LineReceiver):
             Firmware = re.search('Firmware=(.+?),', line).group(1)
             PowerLimit = re.search('PowerLimit=(.+?),', line).group(1)
             evt = {'AlarmCode': AlarmCode, 'Pfwd': Pfwd, 'ref': ref,\
-                 'Texc': Texc, 'Tamp': Tamp, 'Uexc': Uexc, 'Uamp': Uamp,\
-                 'Audio': Audio, 'Iexc': Iexc, 'Iamp': Iamp, 'Uptime': Uptime,\
-                 'Freq': Freq, 'Firmware': Firmware, 'PowerLimit': PowerLimit}
+                'Texc': Texc, 'Tamp': Tamp, 'Uexc': Uexc, 'Uamp': Uamp,\
+                'Audio': Audio, 'Iexc': Iexc, 'Iamp': Iamp, 'Uptime': Uptime,\
+                'Freq': Freq, 'Firmware': Firmware, 'PowerLimit': PowerLimit}
             self.wsMcuFactory.dispatch("http://example.com/mcu#tx-status", evt)
             print evt
-      except AttributeError:
+        except AttributeError:
             print "Could not parse string."
+
 
 ## WS-MCU protocol
 ## knows about websockets
@@ -101,65 +171,68 @@ class McuProtocol(LineReceiver):
 ##
 class WsMcuProtocol(WampServerProtocol):
 
-   def onSessionOpen(self):
-      ## register topic prefix under which we will publish MCU measurements
-      ##
-      self.registerForPubSub("http://example.com/mcu#", True)
+    def onSessionOpen(self):
+        ## register topic prefix under which we will publish MCU measurements
+        ##
+        self.registerForPubSub("http://example.com/mcu#", True)
 
-      ## register methods for RPC
-      ##
-      self.registerForRpc(self.factory.mcuProtocol, "http://example.com/mcu-control#")
+        ## register methods for RPC
+        ##
+        self.registerForRpc(self.factory.mcuProtocol,
+                                    "http://example.com/mcu-control#")
 
 
 ## WS-MCU factory
 ## knows about websockets
-## 
+##
 class WsMcuFactory(WampServerFactory):
 
-   protocol = WsMcuProtocol
+    protocol = WsMcuProtocol
 
-   def __init__(self, url):
-      WampServerFactory.__init__(self, url)
-      self.mcuProtocol = McuProtocol(self)
+    def __init__(self, url):
+        WampServerFactory.__init__(self, url)
+        self.mcuProtocol = McuProtocol(self)
 
 
 if __name__ == '__main__':
 
-   ## parse options
-   ##
-   o = Serial2WsOptions()
-   try:
-      o.parseOptions()
-   except usage.UsageError, errortext:
-      print '%s %s' % (sys.argv[0], errortext)
-      print 'Try %s --help for usage details' % sys.argv[0]
-      sys.exit(1)
+    ## parse options
+    ##
+    o = Serial2WsOptions()
+    try:
+        o.parseOptions()
+    except usage.UsageError, errortext:
+        print '%s %s' % (sys.argv[0], errortext)
+        print 'Try %s --help for usage details' % sys.argv[0]
+        sys.exit(1)
 
-   baudrate = int(o.opts['baudrate'])
-   port = o.opts['port']
-   webport = int(o.opts['webport'])
-   wsurl = o.opts['wsurl']
+    baudrate = int(o.opts['baudrate'])
+    port = o.opts['port']
+    webport = int(o.opts['webport'])
+    wsurl = o.opts['wsurl']
 
-   ## start Twisted log system
-   ##
-   log.startLogging(sys.stdout)
+    ## start Twisted log system
+    ##
+    log.startLogging(sys.stdout)
 
-   ## create Serial2Ws gateway factory
-   ##
-   wsMcuFactory = WsMcuFactory(wsurl)
-   listenWS(wsMcuFactory)               ## calls reactor internally
+    ## create Serial2Ws gateway factory
+    ##
+    wsMcuFactory = WsMcuFactory(wsurl)
+    ## calls reactor internally
+    listenWS(wsMcuFactory)
 
-   ## create serial port and serial port protocol
-   ##
-   log.msg('About to open serial port %s [%d baud] ..' % (port, baudrate))
-   serialPort = SerialPort(wsMcuFactory.mcuProtocol, port, reactor, baudrate = baudrate)
+    ## create serial port and serial port protocol
+    ##
+    log.msg('About to open serial port %s [%d baud] ..' % (port, baudrate))
+    serialPort = SerialPort(wsMcuFactory.mcuProtocol, port,\
+                           reactor, baudrate=baudrate)
 
-   ## create embedded web server for static files
-   ##
-   webdir = File(".")
-   web = Site(webdir)
-   reactor.listenTCP(webport, web)
+    ## create embedded web server for static files
+    ##
+    webdir = File(".")   # Resource
+    web = Site(webdir)
+    reactor.listenTCP(webport, web)
 
-   ## start Twisted reactor ..
-   ##
-   reactor.run()
+    ## start Twisted reactor ..
+    ##
+    reactor.run()
